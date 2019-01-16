6ce275b66ff6207985e7df1bb3dd10f645e31e3c
jimmy zebroski and maddy haldeman <maddy@>
2019-01-16 10:38:17 -0800

    create shout page.

--- Desktop/madlib/js/scripts.js
+++ Desktop/madlib/js/scripts.js
@@ -1,20 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var animalInput = $("input#animal").val();
-    var substanceInput = $("input#substance").val();
-    var verb1Input = $("input#verb1").val();
-    var place1Input = $("input#place1").val();
-    var verb2Input = $("input#verb2").val();
+    var statementInput = $("input#statement").val();
 
-    $(".animal").text(animalInput);
-    $(".substance").text(substanceInput);
-    $(".verb1").text(verb1Input);
-    $(".place1").text(place1Input);
-    $(".verb2").text(verb2Input);
 
-    $("#story").show();
+    $(".statement").text(statementInput.toUpperCase());
+    $("#response").show();
 
     event.preventDefault();
   });
-
 });
